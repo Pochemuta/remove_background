d0efbf9e4b953e8dde8d8886736a349a5667da8f
Сергей Почекутов <ser-pochekutov@yandex.ru>
2023-01-20 14:11:41 +0800

    feat: added tools for removing background

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import os
 from PIL import Image, ImageFile
+from rembg import remove
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+input = os.listdir('./input')
 
 
 for file in input:
     input_path = './input/' + file  # raw file address
     output_path = './output/' + file  # result file address
-    img = Image.open(input_path, 'r')  # open file
-    cover = Image.open('./cover.jpg', 'r')  # open logo
+    img = Image.open(input_path, 'r')  # open photo
+    cover = Image.open('./cover.jpg', 'r')  # open cover
 
     ratio = img.size[0] / img.size[1]  # new ratio
     new_height = 1450
@@ -15,8 +19,11 @@ for file in input:
 
     resize_image = img.resize((new_width, new_height))  # change size
 
-    cover.paste(resize_image, ((cover.width - resize_image.width) // 2,
-                (cover.height - resize_image.height)),
-                mask=resize_image)
+    transparent_image = remove(resize_image)  # remove background
+
+    cover.paste(transparent_image,
+                ((cover.width - transparent_image.width) // 2,
+                 (cover.height - transparent_image.height)),
+                mask=transparent_image)
 
     cover.save(output_path)
